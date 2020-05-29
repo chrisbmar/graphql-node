d0f570cc558231c1b51bb69161e9291ffa7410fb
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-05-29 15:32:34 +0200

    registering graphql with express

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql");
 
 const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true,
+  })
+);
 
 const port = 4000;
 app.listen(port, () => {
